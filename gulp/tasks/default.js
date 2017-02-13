38c6d36f2ca618375344b9121a6d8bb4725f08f6
Guilherme Diego <guidiego.expgames@gmail.com>
2017-02-12 22:17:35 -0200

    Added banners

--- gulp/tasks/default.js
+++ gulp/tasks/default.js
@@ -1,5 +1,5 @@
 gulp.task('default', ["build"], () => {
-    gulp.watch(CONFIG.SRC.CSS, ['css'])
+    gulp.watch('./src/sass/**/*.sass', ['css'])
     gulp.watch(CONFIG.SRC.JS, ['js'])
     gulp.watch('./src/templates/**/*', ['templates'])
     gulp.watch(CONFIG.SRC.IMAGES, ['images'])
